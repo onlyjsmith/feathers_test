5219bb6c79ba96f748312db48362c0aacc75bf4b
Jonathan Smith <jonathan@peoplesized.com>
2018-07-13 16:55:09 +0200

    first, followed tute

---
+++ src/services/messages/messages.hooks.js
@@ -0,0 +1,37 @@
+const {
+  authenticate
+} = require('@feathersjs/authentication').hooks;
+
+const processMessage = require('../../hooks/process-message');
+
+module.exports = {
+  before: {
+    all: [authenticate('jwt')],
+    find: [],
+    get: [],
+    create: [processMessage()],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
